2f36df31edfd0ebfdd51063c8f4272222901a130
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-09-21 11:39:37 -0400

    refactor: moved pagination function to PublicApiController and updated route

--- backend/routes/public/api.php
+++ backend/routes/public/api.php
@@ -2,10 +2,9 @@
 
 use App\Http\Controllers\PublicApiController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentPortfolioController;
 
 //Student Portfolios Pagination
-Route::get('/paginate/student-portfolios', [StudentPortfolioController::class, 'portfolioPaginate']);
+Route::get('/paginate/student-portfolios', [PublicApiController::class, 'portfolioPaginate']);
 
 //Filtering 
 Route::get('/student-portfolios/filter/{roleString}', [PublicApiController::class, 'filterPortfolio']);
