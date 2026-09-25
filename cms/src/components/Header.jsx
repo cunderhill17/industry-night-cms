b1123393191e20a595d05f4adf76560a5245308b
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-09-25 09:34:46 -0400

    feat: add a basic nav bar to the header

--- cms/src/components/Header.jsx
+++ cms/src/components/Header.jsx
@@ -1,7 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
     return (
-        <section className="col-span-full md:col-span-3 lg:col-span-3">
-            <h1>Header</h1>
-        </section>
+        <header className="col-span-full md:col-span-3 lg:col-span-3">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Portfolios</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Projects</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Settings</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
